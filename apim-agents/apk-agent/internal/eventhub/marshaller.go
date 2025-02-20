5c6e29c32ea2ea4e01e1ef6cc8ec28bb8bd36d05
Terance Edmonds <teranceedmonds20@gmail.com>
2025-02-20 14:06:10 +0530

    Added a key manager configuration handler function to Agent interface

--- apim-agents/apk-agent/internal/eventhub/marshaller.go
+++ apim-agents/apk-agent/internal/eventhub/marshaller.go
@@ -19,7 +19,6 @@ package eventhub
 
 import (
 	"github.com/wso2/product-apim-tooling/apim-agent/pkg/eventhub/types"
-	eventhubTypes "github.com/wso2/product-apim-tooling/apim-agent/pkg/eventhub/types"
 	"github.com/wso2/product-apim-tooling/apim-agent/pkg/managementserver"
 	"github.com/wso2/product-apim-tooling/apim-agent/pkg/utils"
 )
@@ -86,17 +85,6 @@ type KeyManager struct {
 	Enabled     bool   `json:"enabled"`
 	Issuer      string `json:"issuer"`
 	Certificate string `json:"certificate"`
-}
-
-// MarshalKeyManagers is used to update the key managers during the startup where
-// multiple key managers are pulled at once. And then it returns the KeyManagerMap.
-func MarshalKeyManagers(keyManagersList *[]eventhubTypes.KeyManager) []eventhubTypes.ResolvedKeyManager {
-	resourceMap := make([]eventhubTypes.ResolvedKeyManager, 0)
-	for _, keyManager := range *keyManagersList {
-		keyManagerSub := MarshalKeyManager(&keyManager)
-		resourceMap = append(resourceMap, keyManagerSub)
-	}
-	return resourceMap
 }
 
 // MarshalMultipleApplications is used to update the applicationList during the startup where
@@ -175,108 +163,6 @@ func marshalKeyMapping(keyMappingInternal *types.ApplicationKeyMapping) manageme
 		SecurityScheme:        "OAuth2",
 		EnvID:                 "Default",
 		Timestamp:             keyMappingInternal.TimeStamp,
-	}
-}
-func marshalKeyManagrConfig(configuration map[string]interface{}) eventhubTypes.KeyManagerConfig {
-	marshalledConfiguration := eventhubTypes.KeyManagerConfig{}
-	if configuration["token_format_string"] != nil {
-		marshalledConfiguration.TokenFormatString = configuration["token_format_string"].(string)
-	}
-	if configuration["issuer"] != nil {
-		marshalledConfiguration.Issuer = configuration["issuer"].(string)
-	}
-	if configuration["ServerURL"] != nil {
-		marshalledConfiguration.ServerURL = configuration["ServerURL"].(string)
-	}
-	if configuration["validation_enable"] != nil {
-		marshalledConfiguration.ValidationEnable = configuration["validation_enable"].(bool)
-	}
-	if configuration["claim_mappings"] != nil {
-		marshalledConfiguration.ClaimMappings = marshalClaimMappings(configuration["claim_mappings"].([]interface{}))
-	}
-	if configuration["grant_types"] != nil {
-		marshalledConfiguration.GrantTypes = marshalGrantTypes(configuration["grant_types"].([]interface{}))
-	}
-	if configuration["OAuthConfigurations.EncryptPersistedTokens"] != nil {
-		marshalledConfiguration.EncryptPersistedTokens = configuration["OAuthConfigurations.EncryptPersistedTokens"].(bool)
-	}
-	if configuration["enable_oauth_app_creation"] != nil {
-		marshalledConfiguration.EnableOauthAppCreation = configuration["enable_oauth_app_creation"].(bool)
-	}
-	if configuration["VALIDITY_PERIOD"] != nil {
-		marshalledConfiguration.ValidityPeriod = configuration["VALIDITY_PERIOD"].(string)
-	}
-	if configuration["enable_token_generation"] != nil {
-		marshalledConfiguration.EnableTokenGeneration = configuration["enable_token_generation"].(bool)
-	}
-	if configuration["issuer"] != nil {
-		marshalledConfiguration.Issuer = configuration["issuer"].(string)
-	}
-	if configuration["enable_map_oauth_consumer_apps"] != nil {
-		marshalledConfiguration.EnableMapOauthConsumerApps = configuration["enable_map_oauth_consumer_apps"].(bool)
-	}
-	if configuration["enable_token_hash"] != nil {
-		marshalledConfiguration.EnableTokenHash = configuration["enable_token_hash"].(bool)
-	}
-	if configuration["self_validate_jwt"] != nil {
-		marshalledConfiguration.SelfValidateJwt = configuration["self_validate_jwt"].(bool)
-	}
-	if configuration["revoke_endpoint"] != nil {
-		marshalledConfiguration.RevokeEndpoint = configuration["revoke_endpoint"].(string)
-	}
-	if configuration["enable_token_encryption"] != nil {
-		marshalledConfiguration.EnableTokenEncryption = configuration["enable_token_encryption"].(bool)
-	}
-	if configuration["RevokeURL"] != nil {
-		marshalledConfiguration.RevokeURL = configuration["RevokeURL"].(string)
-	}
-	if configuration["token_endpoint"] != nil {
-		marshalledConfiguration.TokenURL = configuration["token_endpoint"].(string)
-	}
-	if configuration["certificate_type"] != nil {
-		marshalledConfiguration.CertificateType = configuration["certificate_type"].(string)
-	}
-	if configuration["certificate_value"] != nil {
-		marshalledConfiguration.CertificateValue = configuration["certificate_value"].(string)
-	}
-	if configuration["consumer_key_claim"] != nil {
-		marshalledConfiguration.ConsumerKeyClaim = configuration["consumer_key_claim"].(string)
-	}
-	if configuration["scopes_claim"] != nil {
-		marshalledConfiguration.ScopesClaim = configuration["scopes_claim"].(string)
-	}
-	return marshalledConfiguration
-}
-func marshalGrantTypes(grantTypes []interface{}) []string {
-	resolvedGrantTypes := make([]string, 0)
-	for _, grantType := range grantTypes {
-		if resolvedGrantType, ok := grantType.(string); ok {
-			resolvedGrantTypes = append(resolvedGrantTypes, resolvedGrantType)
-		}
-	}
-	return resolvedGrantTypes
-
-}
-func marshalClaimMappings(claimMappings []interface{}) []eventhubTypes.Claim {
-	resolvedClaimMappings := make([]eventhubTypes.Claim, 0)
-	for _, claim := range claimMappings {
-		if resolvedClaim, ok := claim.(eventhubTypes.Claim); ok {
-			resolvedClaimMappings = append(resolvedClaimMappings, resolvedClaim)
-		}
-	}
-	return resolvedClaimMappings
-}
-
-// MarshalKeyManager is used to map Internal key manager
-func MarshalKeyManager(keyManagerInternal *types.KeyManager) eventhubTypes.ResolvedKeyManager {
-	return eventhubTypes.ResolvedKeyManager{
-		UUID:             keyManagerInternal.UUID,
-		Name:             keyManagerInternal.Name,
-		Enabled:          keyManagerInternal.Enabled,
-		Type:             keyManagerInternal.Type,
-		Organization:     keyManagerInternal.Organization,
-		TokenType:        keyManagerInternal.TokenType,
-		KeyManagerConfig: marshalKeyManagrConfig(keyManagerInternal.Configuration),
 	}
 }
 
--- apim-agents/apk-agent/main.go
+++ apim-agents/apk-agent/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"github.com/wso2/product-apim-tooling/apim-agent/config"
+	"github.com/wso2/product-apim-tooling/apim-agent/pkg/eventhub/types"
+	msg "github.com/wso2/product-apim-tooling/apim-agent/pkg/messaging"
 	"github.com/wso2/product-apim-tooling/apim-agents/apk-agent/internal/agent"
 	"github.com/wso2/product-apim-tooling/apim-agents/apk-agent/internal/events"
-	"github.com/wso2/product-apim-tooling/apim-agents/apk-agent/internal/messaging"
+	"github.com/wso2/product-apim-tooling/apim-agents/apk-agent/internal/loggers"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -26,9 +28,7 @@ func (a Agent) Run(conf *config.Config, mgr manager.Manager) {
 }
 
 // ProcessEvents handles gateway specific functions need to be triggered on event processing
-func (a Agent) ProcessEvents(conf *config.Config, client client.Client) {
-	messaging.ProcessEvents(conf, client)
-}
+func (a Agent) ProcessEvents(conf *config.Config, client client.Client) {}
 
 // HandleLifeCycleEvents handles the events of an api through out the life cycle
 func (a Agent) HandleLifeCycleEvents(data []byte) {
@@ -38,32 +38,38 @@ func (a Agent) HandleLifeCycleEvents(data []byte) {
 
 // HandleAPIEvents to process api related data
 func (a Agent) HandleAPIEvents(data []byte, eventType string, conf *config.Config, client client.Client) {
-	fmt.Println("Triggered: HandleAPIEvents")
+	loggers.LoggerAgent.Println("Triggered: HandleAPIEvents")
 	events.HandleAPIEvents(data, eventType, conf, client)
 }
 
 // HandleApplicationEvents to process application related events
 func (a Agent) HandleApplicationEvents(data []byte, eventType string, client client.Client) {
-	fmt.Println("Triggered: HandleApplicationEvents")
+	loggers.LoggerAgent.Println("Triggered: HandleApplicationEvents")
 	events.HandleApplicationEvents(data, eventType, client)
 }
 
 // HandleSubscriptionEvents to process subscription related events
 func (a Agent) HandleSubscriptionEvents(data []byte, eventType string, client client.Client) {
-	fmt.Println("Triggered: HandleSubscriptionEvents")
+	loggers.LoggerAgent.Println("Triggered: HandleSubscriptionEvents")
 	events.HandleSubscriptionEvents(data, eventType, client)
 }
 
 // HandlePolicyEvents to process policy related events
 func (a Agent) HandlePolicyEvents(data []byte, eventType string, client client.Client) {
-	fmt.Println("Triggered: HandlePolicyEvents")
+	loggers.LoggerAgent.Println("Triggered: HandlePolicyEvents")
 	events.HandlePolicyEvents(data, eventType, client)
 }
 
 // HandleAIProviderEvents to process AI Provider related events
 func (a Agent) HandleAIProviderEvents(data []byte, eventType string, client client.Client) {
-	fmt.Println("Triggered: HandleAIProviderEvents")
+	loggers.LoggerAgent.Println("Triggered: HandleAIProviderEvents")
 	events.HandleAIProviderEvents(data, eventType, client)
+}
+
+// HandleKMConfiguration to handle Key Manager configurations
+func (a Agent) HandleKMConfiguration(keyManager *types.KeyManager, notification msg.EventKeyManagerNotification, client client.Client) {
+	loggers.LoggerAgent.Println("Triggered: HandleKMConfiguration")
+	events.HandleKMConfiguration(keyManager, notification, client)
 }
 
 // AgentPlugin exports the agent as a variable
